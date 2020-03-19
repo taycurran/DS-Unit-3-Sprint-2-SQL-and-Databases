3169ecd65569cfe5f88301af0caef4d4f184c9fb
taycurran <taylorcurranagtc@gmail.com>
2020-03-19 15:41:48 -0500

    Adding Notes to SQL Connection Process

--- module2-sql-for-analysis/app/pgSQL_conn_test.py
+++ module2-sql-for-analysis/app/pgSQL_conn_test.py
@@ -1,5 +1,9 @@
+# This mod allows us to communicate with PostgreSQL
 import psycopg2
+# The Error class allows the app to understand errors in greater detail.
+# This class returns an error message with an error code.
 from psycopg2 import Error
+# TODO - Write notes on dotenv and os modules
 from dotenv import load_dotenv
 import os
 
@@ -11,17 +15,28 @@ DB_USER = os.getenv("DB_USER", default="OOPS")
 DB_PASSWORD = os.getenv("DB_PASSWORD", default="OOPS")
 DB_HOST = os.getenv("DB_HOST", default="OOPS")
 
+# We place our code in the try-except block to catch the database exceptions and errors
+# that may occur during the process.
+
 # Initial Test for Connection
 try:
+    # create a connection to our PostgreSQL database instance
+    # the connect method accepts various enviornment variables as arguments
     connection = psycopg2.connect(user=DB_USER,
                                   password=DB_PASSWORD,
                                   host=DB_HOST,
                                   database=DB_NAME)
+    # create cursor object allowing us to execute PostgreSQL 
+    # command through Python source code.
     cursor = connection.cursor()
+    # The connection object has an attribute to get connection properties
     # Print PostgresSQL Connection Properties
     print("CONNECTION PROPERTIES:", connection.get_dsn_parameters(), "\n")
 
     # Print PostgreSQL version
+    # Cursor's excecute method allows us to execute a database query or operation
+    # The excecute method thakes a SQL query as an argument.
+    # Cursor's method fetchall, fetchone, or fetchmany() allow us retrieve query results
     cursor.execute("SELECT version();")
     record = cursor.fetchone()
     print("You are connected to - ", record, "\n")
@@ -31,6 +46,8 @@ except (Exception, psycopg2.Error) as error:
 
 finally:
     # closing DB connection
+    # It is always good practice to close the cursor and connection object
+    # once your work gets completed to avoid DB issues.
     if(connection):
         cursor.close()
         connection.close()
